9abb6b45db7fa252a4d26464f6a0545ead31ac51
MurodFikriFadlurohman <murodadipate@gmail.com>
2024-05-07 16:22:25 +0700

    push

---
+++ Kuliah/latihan/admin.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    <h2>Selamat Datang Admin</h2>
+    <button><a href="login.php">Logout</a></button>
+</body>
+</html>
---
+++ uts/soal4.php
@@ -0,0 +1,32 @@
+<!-- Contoh Array numerik -->
+<?php
+$buah = ["apel", "jeruk", "pisang", "anggur"];
+
+echo $buah[0];
+echo "<br>";
+echo $buah[1];
+echo "<br>";
+echo $buah[2];
+echo "<br>";
+echo $buah[3];
+echo "<br>";
+?>
+
+
+
+<!-- Contoh Array Associative -->
+<?php
+$mahasiswa = [
+        "nama" => "Murod Fikri Fadlurohman",
+        "nrp" => "233040090",
+        "jurusan" => "Teknik Informatika",
+];
+
+echo $mahasiswa["nama"];
+echo "<br>";
+echo $mahasiswa["nrp"];
+echo "<br>";
+echo $mahasiswa["jurusan"];
+echo "<br>";
+
+?>
